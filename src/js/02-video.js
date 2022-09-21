c79a9d82a281502a61eb00ad833729e5c54fcf44
alexander-kulyk <kulyk.alexander@gmail.com>
2022-09-21 18:07:41 +0300

    update vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import VimeoPlayer from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+const CURRENTTIME_KEY = "videoplayer-current-time"
+
+
+const onPlay = function(data) {
+    const currentTimeValue = JSON.stringify(data);
+    
+    localStorage.setItem(CURRENTTIME_KEY, currentTimeValue)
+    
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+
+const getItemTime = localStorage.getItem(CURRENTTIME_KEY);
+const seconds = JSON.parse(getItemTime).seconds;
+
+player.setCurrentTime(seconds).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
