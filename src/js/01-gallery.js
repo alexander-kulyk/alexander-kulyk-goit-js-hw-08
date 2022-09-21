854ae1251d6afdd58fed3efc91d8a8d3cfedb520
alexander-kulyk <kulyk.alexander@gmail.com>
2022-09-21 14:37:15 +0300

    update gellary

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = createGalleryItemsMarkup(galleryItems);
+
+
+//----Створення розмітки-------------------
+
+function createGalleryItemsMarkup(gallery) {
+
+    const galleryMarkup = gallery.reduce((acc,{preview, original, description}) =>{
+      return acc + `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    }, '');
+    
+    return galleryMarkup;
+};
+
+//---галерея SimpleLightbox---------------
+
+const lightbox = new SimpleLightbox('.gallery .gallery__item', {captionsData:'alt',captionDelay: 250});
+
